f1542b4d2b25c807d24a720d57665270ae9855ff
eviter <63437856+eviter@users.noreply.github.com>
2021-11-21 00:01:12 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
-import 'package:faality/screens/wrapper.dart';
-import 'package:faality/services/auth.dart';
+import 'package:faality/screens/home/nav.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'package:faality/models/user.dart' as u;
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<u.User?>.value(
-      initialData: null,
-      value: AuthService().user,
-      child: MaterialApp(
-        home: Wrapper(),
+    return MaterialApp(
+        home: nav(),
         debugShowCheckedModeBanner: false,
-      ),
     );
   }
 }
